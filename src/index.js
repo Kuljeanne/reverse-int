431a8758aa9d4e5d4a431ce0cbe275961e4b61bd
Zhanna Kulaginae <kuljeanne@gmail.com>
2023-01-05 13:19:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result;
+    if (n % 10 === 0) {
+        n /= 10
+    };
+    let digits = String(n);
+    if (n < 0) {
+        digits = digits.split('-')
+        digits[1] = digits[1].split('').reverse().join('')
+        result = digits.join('')
+    } else {
+        digits = digits.split('').reverse();
+        result = digits.join('')
+    }
+    return result
 }
